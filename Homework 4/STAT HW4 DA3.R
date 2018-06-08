39a2430a38a0f649137002cab1253f5f09456c7c
Tarek Tohme <tpt00@mail.aub.edu>
2018-06-08 21:41:16 +0300

    added hclust and started DA3 finalization

--- Homework 4/STAT HW4 DA3.R
+++ Homework 4/STAT HW4 DA3.R
@@ -2,6 +2,7 @@
 
 #### K-MEANS CLUSTERING ####
 library(scatterplot3d)
+library(randomForest)
 
 set.seed(42)
 train = read.table("test.txt", header = T)
@@ -19,7 +20,7 @@ PC1 = pcr.model$x[,"PC1"]
 PC2 = pcr.model$x[,"PC2"]
 PC3 = pcr.model$x[,"PC3"]
 
-scatterplot3d(PC1, PC2, PC3, color = colors, main = "k-means clustering with two classes")
+scatterplot3d(PC1, PC2, PC3, color = colors, main = "k-means clustering with two classes", type="h", angle = 70)
 plot(x=PC1, y=PC2,  ylab = "PC2", col = colors, main = "k-means clustering with two classes")
 
 # let's find out if the clusters correspond to the y values
@@ -27,3 +28,20 @@ km.err = sum(Y != c) # 7! pretty impressive
 
 
 #### HIERARCHICAL CLUSTERING ####
+# euclidean distance
+hc.model = hclust(dist(X), method="complete")
+plot(hc.model, cex=0.5, main="complete linkage and euclidean distance")
+
+# random forest distance
+rf.model = randomForest(X, proximity = TRUE, importance = TRUE)
+rf.prox = rf.model$proximity
+
+hc.model2 = hclust(as.dist(rf.prox), method="complete")
+plot(hc.model2, cex=0.5, main="complete linkage and RF distance")
+
+# correlation-based distance
+cor = cor(X)[2:22, 2:22]
+hc.model3 = hclust(as.dist(cor), method="complete")
+plot(hc.model3, cex=1, main="complete linkage and correlation-based distance")
+
+
